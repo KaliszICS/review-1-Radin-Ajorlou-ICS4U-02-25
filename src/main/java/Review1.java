0f4993b32feefa07b99127ba445545472ddc351b
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-02-07 00:35:14 +0000

    done

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,33 +2,33 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
-		System.out.print("There once was a man from St. Ives.");
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		int num1=9;
 		int num2=5;
 		System.out.println(num1/num2);
-		System.out.print(num1%num2);
+		System.out.println(num1%num2);
 				//Write question 2 code here
 	}
 
 	public static void q3() {
 		//Write question 3 code here
-		System.out.print("Hello/nHello again");
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
-		bool correct = true;
-		Systout.println(correct);
+		boolean correct = true;
+		System.out.println(correct);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
 		String hi = "Hello";
-		String earth="world";
-		system.out.print(hi+" "+earth);
+		String earth="World";
+		System.out.println(hi+" "+earth);
 	}
 
 }
